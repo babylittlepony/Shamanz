589bfd2208b86cac474699ae61cc2a5fc9bc5aa1
Egar <pramana.egar1540@gmail.com>
2023-01-20 08:26:28 +0700

    added song & artists tracks

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -9,6 +9,7 @@ const SongCard = ({ song, i }) => {
 
   return (
     <div className="flex w-[250px] animate-slideup cursor-pointer flex-col rounded-lg bg-white/5 bg-opacity-80 p-4 backdrop-blur-sm">
+      {/* Image tracks */}
       <div className="group relative h-56 w-full">
         <div
           className={`absolute inset-0 items-center justify-center bg-black bg-opacity-50 group-hover:flex ${
@@ -20,6 +21,23 @@ const SongCard = ({ song, i }) => {
           <PlayPause />
         </div>
         <img src={song.images?.coverart} alt="song_img" />
+      </div>
+      {/* Song & Artist tracks */}
+      <div className="mt-4 flex flex-col">
+        <p className="truncate text-lg font-semibold text-white">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="mt-1 truncate text-sm text-gray-300">
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : "/top-artists"
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
       </div>
     </div>
   )
